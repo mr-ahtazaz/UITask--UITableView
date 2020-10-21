a1c26bc4be41727598dbab6afc1eb881d3a998e7
mr_ahtazaz <mr.ahtazaz@gmail.com>
2020-10-21 12:39:55 +0500

    Initial Commit

---
+++ UI-Task-2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UI-Task-2
+//
+//  Created by Ahtazaz on 21/10/2020.
+//  Copyright © 2020 Ahtazaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
